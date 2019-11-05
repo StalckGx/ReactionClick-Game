b379ced15855fb8e963a2013e54bf1b0a28f326d
StalckGx <vinny.polsom@outlook.com>
2019-11-05 17:07:26 -0300

    Project Setup

---
+++ script.js
@@ -0,0 +1,7 @@
+const container = document.querySelector(".container");
+const startGame = document.querySelector(".startGame");
+const scoreArea = document.querySelector(".score");
+
+let player = {
+    score:0
+};
