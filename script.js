61bcd4646cc0281634ac27487e75d204acd81e8e
StalckGx <vinny.polsom@outlook.com>
2019-11-06 10:28:01 -0300

    Event Handling Element Added

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ let player = {
 
 startGame.addEventListener("click", function() {
     startGame.style.display = "none";
+    scoreArea.innerHTML = "";
+
     let randTime = Math.random() * 2000 + 1000;
     setTimeout(makeitem, randTime);
 })
@@ -19,12 +21,20 @@ function makeitem() {
     div.style.position = "absolute";
     div.style.left = Math.random() * boundary.width + "px";
     div.style.top = Math.random() * boundary.height + "px";
-    div.style.width = Math.random() * 16 + 48 + "px";
-    div.style.height = Math.random() * 16 + 48 + "px";
+    div.style.width = Math.random() * 10 + 40 + "px";
+    div.style.height = Math.random() * 10 + 40 + "px";
     div.style.cursor = "pointer";
     div.style.backgroundColor = "#" + Math.random().toString(16).substr(-6);
     div.style.border = "2px solid black";
     div.startTime = Date.now();
+
+    div.addEventListener("click", function() {
+        let endTime = Date.now();
+        let difference = (endTime - div.startTime) / 1000;
+        scoreArea.innerHTML = "Clicked in " + difference + " seconds!";
+        startGame.style.display = "inline-block";
+        container.removeChild(div);
+    })
     
     container.appendChild(div);
 }
